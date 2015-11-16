fbc2f738002625ca234add693bd427d200a442c9
Jack Scott <jack.rob.scott@gmail.com>
2015-11-16 21:31:21 +0800

    create page generator

---
+++ src/page/index.js
@@ -0,0 +1,111 @@
+const path = require('path');
+const yosay = require('yosay');
+const _ = require('lodash');
+const Base = require('../base');
+
+module.exports = class Generator extends Base {
+  constructor() {
+    super(...arguments);
+
+    this.option('skip-welcome-message', {
+      desc: 'Skips the welcome message',
+      type: Boolean,
+    });
+
+    this.option('markups', {
+      desc: 'Which markup compiler to use in page',
+      type: String
+    });
+
+    this.option('styles', {
+      desc: 'Which style compiler to use in page',
+      type: Boolean
+    });
+  }
+
+  initializing() {
+    this.data = {
+      name: '',
+      markups: this.options.markups,
+      styles: this.options.styles,
+      yorc: this.config.getAll(),
+      site: this.fs.readJSON(this.destinationPath('config.json')),
+    };
+  }
+
+  prompting() {
+    let done = this.async();
+
+    if (!this.options['skip-welcome-message']) {
+      this.log(yosay('Let\'s make a new page.'));
+    }
+
+    const prompts = [{
+      type: 'checkbox',
+      name: 'markups',
+      message: 'Page markup type:',
+      choices: [{
+        name: 'html',
+        value: 'HTML',
+      }, {
+        name: 'jade',
+        value: 'Jade',
+      }, {
+        name: 'nunjucks',
+        value: 'Nunjucks',
+      }],
+      when: !this.data.markups || ['html', 'jade', 'nunjucks'].indexOf(this.data.markups) === -1,
+    }, {
+      type: 'checkbox',
+      name: 'styles',
+      message: 'Page style type:',
+      choices: [{
+        name: 'css',
+        value: 'CSS',
+      }, {
+        name: 'sass',
+        value: 'SASS',
+      }, {
+        name: 'scss',
+        value: 'SCSS',
+      }, {
+        name: 'less',
+        value: 'Less',
+      }],
+      when: !this.data.styles || ['css', 'sass', 'scss', 'less'].indexOf(this.data.styles) === -1,
+    }];
+
+    this.prompt(prompts, (answers) => {
+      _.assign(this.data, answers);
+
+      done();
+    });
+  }
+
+  writing() {
+    let out = path.join(this.data.site.src, this.data.name);
+    switch (this.data.markups) {
+      case 'jade':
+        this._copyDirectory('jade', out);
+        break;
+      case 'nunjucks':
+        this._copyDirectory('nunjucks', out);
+        break;
+      default:
+        this._copyDirectory('html', out);
+    }
+    switch (this.data.styles) {
+      case 'sass':
+        this._copyDirectory('sass', out);
+        break;
+      case 'scss':
+        this._copyDirectory('scss', out);
+        break;
+      case 'less':
+        this._copyDirectory('less', out);
+        break;
+      default:
+        this._copyDirectory('css', out);
+    }
+  }
+};
