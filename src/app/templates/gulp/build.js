d7bea61c6f658cd4b01fcdaf836c54a79c1d73c7
Jack Scott <jack.rob.scott@gmail.com>
2015-11-16 19:26:20 +0800

    add injection of scripts

--- src/app/templates/gulp/build.js
+++ src/app/templates/gulp/build.js
@@ -1,27 +1,39 @@
 'use strict';
 
+var path = require('path');
 var gulp = require('gulp');
 var filter = require('gulp-filter');
+var inject = require('gulp-inject');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var csso = require('gulp-csso');
 var minify = require('gulp-minify-html');
+var sequence = require('run-sequence');
 var del = require('del');
 var config = require('./config');
 var help = require('./gulp/help');
 
-gulp.task('build', [
-  'clean:build',
+gulp.task('build', ['clean:build'], function(cb) {
+  sequence('compress', 'inject:build', cb);
+});
+
+gulp.task('compress', [
   'compile',
 ], function() {
   var htmlFilter = filter('**/*.html', {
-    restore: true
+    restore: true,
   });
-  var jsFilter = filter('**/*.js', {
-    restore: true
+  var jsFilter = filter(['**/*.js', '!vendor/*.js'], {
+    restore: true,
   });
-  var cssFilter = filter('**/*.css', {
-    restore: true
+  var jsVendor = filter('vendor/*.js', {
+    restore: true,
+  });
+  var cssFilter = filter(['**/*.css', '!vendor/*.css'], {
+    restore: true,
+  });
+  var cssVendor = filter('vendor/*.css', {
+    restore: true,
   });
 
   return gulp.src(help.filter(config.tmp, ''))
@@ -36,10 +48,45 @@ gulp.task('build', [
       .pipe(concat('scripts.min.js'))
       .pipe(uglify())
     .pipe(jsFilter.restore)
+    .pipe(jsVendor)
+      .pipe(concat('vendor.min.js'))
+      .pipe(uglify())
+    .pipe(jsVendor.restore)
     .pipe(cssFilter)
       .pipe(concat('styles.min.css'))
       .pipe(csso())
     .pipe(cssFilter.restore)
+    .pipe(cssVendor)
+      .pipe(concat('vendor.min.css'))
+      .pipe(csso())
+    .pipe(cssVendor.restore)
+    .pipe(gulp.dest(config.dist));
+});
+
+gulp.task('inject:build', function() {
+  var sources = gulp.src([
+    path.join(config.dist, '**/*.js'),
+    path.join(config.dist, '**/*.css'),
+    '!' + path.join(config.dist, 'vendor/*.js'),
+    '!' + path.join(config.dist, 'vendor/*.css'),
+  ], {
+    read: false,
+  });
+  var vendor = gulp.src([
+    path.join(config.dist, 'vendor/*.js'),
+    path.join(config.dist, 'vendor/*.css'),
+  ], {
+    read: false,
+  });
+
+  return gulp.src(path.join(config.dist, '**/*.html'))
+    .pipe(inject(sources, {
+      relative: true,
+    }))
+    .pipe(inject(vendor, {
+      relative: true,
+      name: 'bower',
+    }))
     .pipe(gulp.dest(config.dist));
 });
 
--- src/app/templates/gulp/compile.js
+++ src/app/templates/gulp/compile.js
@@ -1,6 +1,9 @@
 'use strict';
 
+var path = require('path');
 var gulp = require('gulp');
+var bowerFiles = require('main-bower-files');
+var inject = require('gulp-inject');
 var del = require('del');
 var sequence = require('run-sequence');
 var config = require('../config');
@@ -11,9 +14,42 @@ gulp.task('compile', ['clean:compile'], function(cb) {
     'markups',
     'scripts',
     'styles',
-    'images', 
+    'images',
     'other',
-  ], cb);
+    'vendor',
+  ], 'inject:compile', cb);
+});
+
+gulp.task('vendor', function() {
+  return gulp.src(bowerFiles())
+    .pipe(gulp.dest(path.join(config.tmp, 'vendor')));
+});
+
+gulp.task('inject:compile', function() {
+  var sources = gulp.src([
+    path.join(config.tmp, '**/*.js'),
+    path.join(config.tmp, '**/*.css'),
+    '!' + path.join(config.tmp, 'vendor/*.js'),
+    '!' + path.join(config.tmp, 'vendor/*.css'),
+  ], {
+    read: false,
+  });
+  var vendor = gulp.src([
+    path.join(config.tmp, 'vendor/*.js'),
+    path.join(config.tmp, 'vendor/*.css'),
+  ], {
+    read: false,
+  });
+
+  return gulp.src(path.join(config.tmp, '**/*.html'))
+    .pipe(inject(sources, {
+      relative: true,
+    }))
+    .pipe(inject(vendor, {
+      relative: true,
+      name: 'bower',
+    }))
+    .pipe(gulp.dest(config.tmp));
 });
 
 gulp.task('clean:compile', function() {
